28d235006e08947190e920646d3807010f07a7de
Rocky <rrsalerno21@gmail.com>
2020-06-29 10:39:32 -0700

    added put to routes

--- restart-folder/routes/apiRoutes.js
+++ restart-folder/routes/apiRoutes.js
@@ -26,6 +26,21 @@ router.get('/find/:id', (req, res) => {
     }).then(todo => res.json(todo));
 })
 
+// Update a to do
+router.put('/edit', (req, res) => {
+    db.Todo.update(
+        {
+            task_header: req.body.task_header,
+            task_details: req.body.task_details,
+            status: req.body.status,
+            folder: req.body.folder
+        }, 
+        {
+            where: {id: req.body.id}
+        }
+    ).then(()=> res.send('Success'));
+})
+
 
 
 module.exports = router;
