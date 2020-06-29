e146fcb5f68d75727968480cbae994b1d638ce3a
Rocky <rrsalerno21@gmail.com>
2020-06-29 09:52:57 -0700

    added todo model

---
+++ restart-folder/models/todo.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+    const Todo = sequelize.define('Todo', {
+        task_header: {
+            type: DataTypes.STRING,
+            primaryKey: true
+        },
+        task_details: {
+            type: DataTypes.STRING
+        },
+        status: {
+            type: DataTypes.BOOLEAN
+        },
+        folder: {
+            type: DataTypes.STRING,
+            defaultValue: 'Standard',
+        }
+    }, {
+        freezeTableName: true
+    });
+    return Todo;
+}
