037655a654fc6f9139712e26004b5b8a7ec3f1d9
Rocky <rrsalerno21@gmail.com>
2020-06-30 11:27:33 -0700

    added count to root route

--- restart-folder/routes/htmlRoutes.js
+++ restart-folder/routes/htmlRoutes.js
@@ -10,9 +10,23 @@ const scheme = {
 // Root to get all data needed to render
 router.get('/', (req, res) => {
     db.Todo.findAll().then(todos => {
+        // Make todos into a JSON object
         let todosAsJSON = Serializer.serializeMany(todos, db.Todo, scheme);
         console.log(todosAsJSON);
-        res.render('index', {todos: todosAsJSON})
+
+        // get completed task count
+        let completedCount = 0;
+        for (task of todosAsJSON) {
+            if (task.status) {
+                completedCount ++;
+            }
+        };
+
+        res.render('index', {
+            todos: todosAsJSON,
+            activeCount: todosAsJSON.length - completedCount,
+            completedCount: completedCount
+        })
     });
 });
 
