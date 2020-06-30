9c29119c7b087ee7053a72c97d9ee36ec0440efb
Rocky <rrsalerno21@gmail.com>
2020-06-30 10:51:27 -0700

    added hndlbrs render basics with Serializer pckge

--- restart-folder/routes/htmlRoutes.js
+++ restart-folder/routes/htmlRoutes.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
+const Serializer = require('sequelize-to-json');
 
-// Root
+const scheme = {
+    include: ['@all']
+}
+
+// Root to get all data needed to render
 router.get('/', (req, res) => {
     db.Todo.findAll().then(todos => {
-        res.render('index', todos);
+        let todosAsJSON = Serializer.serializeMany(todos, db.Todo, scheme);
+        console.log(todosAsJSON);
+        res.render('index', {todos: todosAsJSON})
     });
-})
+});
+
 
 module.exports = router;
