4f186174c4ac93b1e5ea655cdea45f9311011bdf
Rocky <rrsalerno21@gmail.com>
2020-06-30 16:11:33 -0700

    added renderAfterChange fn to play with

--- restart-folder/public/assets/js/toDoListApp.js
+++ restart-folder/public/assets/js/toDoListApp.js
@@ -53,16 +53,18 @@ $('#modal-act-button').on('click', function(event) {
     // if adding
     if (method === 'ADD') {
         // create the new task as an object
-            const newTask = {
-                task_header: $(".task-name").val().trim(),
-                task_details: $(".task-body").val().trim(),
-                status: taskStatus,
-                folder: 'Standard'
-            }
-        // do a post to the server and then reload the page
+        const newTask = {
+            task_header: $(".task-name").val().trim(),
+            task_details: $(".task-body").val().trim(),
+            status: taskStatus,
+            folder: 'Standard'
+        }
+        
+        // do a post to the server and then render the page
         $.post('/api/new', newTask)
             .then(() => location.reload());
 
+        
     } else if (method === 'EDIT') {
         // edit the post
         const editedTask = {
@@ -83,8 +85,14 @@ $('#modal-act-button').on('click', function(event) {
             location.reload();
         });
     }
+
+    // clear input values
+    $('.task-name').val('');
+    $('.task-body').val('');
+
 });
 
+// Delete a task
 $('.delete-task-btn').on('click', function(event) {
     console.log('you clicked me');
     let id = $(this).data('taskid');
@@ -134,4 +142,71 @@ $('.undo-task-btn').on('click', function(event) {
         location.reload();
     })
 })
+
+
+async function renderAfterChange() {
+    // get updated data
+    const updatedData = await $.get('/api/all');
+
+    // separate the data based on status
+    const activeList = updatedData.filter(task => {
+        return !task.status;
+    });
+
+    const completedList = updatedData.filter(task => {
+        return task.status;
+    });
+
+    // empty the divs
+    $('#activeListDiv').empty();
+    $('#completedListDiv').empty();
+    
+    // loop through each array and append to divs
+    for (task of activeList) {
+        let html = `
+        <div class="list-group-item list-group-item-action">
+    <div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1">${task.task_header}</h5>
+        <small>${task.updatedAt}</small>
+    </div>
+
+    <div class="d-sm-flex w-100 justify-content-between">
+        <p class="mb-1">${task.task_details}</p>
+        <span class="icon-container d-flex  justify-content-center">
+            <i data-taskId="${task.id}" class="btn fas fa-check-square text-success icon-med-size complete-task-btn"></i>
+            <i class="btn fas fa-edit text-muted icon-med-size edit-task-btn" type="button" data-toggle="modal" data-target="#taskModal" data-taskId="${task.id}" data-whatever="EDIT"></i>
+            <i class="btn fas fa-trash text-danger icon-med-size delete-task-btn" data-taskId="${task.id}"></i>
+        </span>
+    </div>
+</div>`
+        $('#activeListDiv').append(html);
+    };
+
+    for (task of activeList) {
+        let html = `
+        <div class="list-group-item list-group-item-action">
+    <div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1"><del>${task.task_header}<del></h5>
+        <small>${task.updatedAt}</small>
+    </div>
+
+    <div class="d-sm-flex w-100 justify-content-between">
+        <p class="mb-1"><del>${task.task_details}<del></p>
+        <span class="icon-container d-flex  justify-content-center">
+        <i data-taskId="${task.id}" class="btn fas fa-undo text-success icon-med-size undo-task-btn"></i>
+            <i class="btn fas fa-edit text-muted icon-med-size edit-task-btn" type="button" data-toggle="modal" data-target="#taskModal" data-taskId="${task.id}" data-whatever="EDIT"></i>
+            <i class="btn fas fa-trash text-danger icon-med-size delete-task-btn" data-taskId="${task.id}"></i>
+        </span>
+    </div>
+</div>`
+        $('#completedListDiv').append(html);
+    };
+
+    // exit out of module if open
+    if ($('#taskModal').hasClass('show')) {
+        $('.modal-header button').click();
+    }
+}
+
+
 
